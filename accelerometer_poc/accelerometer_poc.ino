d389a9714cd6fc0f8b984036f9c7bda6843730d6
Sanju Prakash Kannioth <Sanju.Kannioth@colorado.edu>
2019-09-13 21:25:09 -0600

    Add accelerometer POC code (Arduino)

---
+++ accelerometer_poc/accelerometer_poc.ino
@@ -0,0 +1,59 @@
+/*
+  Code edited from SparkFun MMA8452Q library
+  Edited code author: Sanju Prakash Kannioth
+  
+  Library for the MMA8452Q
+  By: Jim Lindblom and Andrea DeVore
+  SparkFun Electronics
+
+  This sketch uses the SparkFun_MMA8452Q library to initialize
+  the accelerometer and stream calcuated x, y, z, acceleration
+  values from it (in g units).
+
+  Hardware hookup:
+  Arduino --------------- MMA8452Q Breakout
+    3.3V  ---------------     3.3V
+    GND   ---------------     GND
+  SDA (A4) --\/330 Ohm\/--    SDA
+  SCL (A5) --\/330 Ohm\/--    SCL
+
+  The MMA8452Q is a 3.3V max sensor, so you'll need to do some
+  level-shifting between the Arduino and the breakout. Series
+  resistors on the SDA and SCL lines should do the trick.
+
+  License: This code is public domain, but if you see me
+  (or any other SparkFun employee) at the local, and you've
+  found our code helpful, please buy us a round (Beerware
+  license).
+
+  Distributed as is; no warrenty given.
+*/
+
+#include <Wire.h>                 // Must include Wire library for I2C
+#include "SparkFun_MMA8452Q.h"    // Click here to get the library: http://librarymanager/All#SparkFun_MMA8452Q
+
+MMA8452Q accel;                   // create instance of the MMA8452 class
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("MMA8452Q Basic Reading Code!");
+  Wire.begin();
+
+  if (accel.begin() == false) {
+    Serial.println("Not Connected. Please check connections and read the hookup guide.");
+    while (1);
+  }
+}
+
+void loop() {
+  if (accel.available()) {      // Wait for new data from accelerometer
+      Serial.print("Pouring");
+      Serial.println();
+    }
+
+    if(accel.getCalculatedX() > 0.5 or accel.getCalculatedY() > 0.5 or accel.getCalculatedX() < -0.5 or accel.getCalculatedY() < -0.5 ){
+      Serial.print("Drinking");
+      Serial.println();
+    }
+  }
+}
